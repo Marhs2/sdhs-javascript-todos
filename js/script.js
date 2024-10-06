fef44f8e8ae52a21a4cf0f96ab2c02238ff2ceff
Marhs2 <wjdehddfl12@gmail.com>
2024-10-06 21:02:07 +0900

    완료 기능 추가

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ const $input = document.querySelector('.new-todo');
 const $toDoBox = document.querySelector('.todo-list');
 
 function createToDo() {
-  if ($input.value === '') {
-    alert('입력하세요');
-  } else {
-    $toDoBox.innerHTML += `
+    if ($input.value === '') {
+        alert('입력하세요');
+    } else {
+        $toDoBox.innerHTML += `
     <li>
       <div class="view">
         <input class="toggle" type="checkbox" />
@@ -14,22 +14,39 @@ function createToDo() {
       </div>
       </li>
     `;
-    $input.value = '';
-  }
+        $input.value = '';
+    }
 }
 
 function DelFun() {
-  const $delButtons = document.querySelectorAll('.destroy');
-  $delButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        button.parentNode.parentNode.remove()
+    const $delButtons = document.querySelectorAll('.destroy');
+    $delButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            button.parentNode.parentNode.remove()
+        });
     });
-  });
 }
-DelFun()
 
+function AddCompleted() {
+    const $CompleteBtn = document.querySelectorAll('.toggle');
+    $CompleteBtn.forEach((button) => {
+        button.addEventListener('click', () => {
+            if(button.parentNode.parentNode.classList.contains('completed')){
+                button.parentNode.parentNode.classList.remove('completed')
+            }else{
+                button.parentNode.parentNode.classList.add('completed')
+            }
+            
+        });
+    });
+}
 $input.addEventListener('keypress', (e) => {
-  if (e.keyCode === 13) {
-    createToDo();
-  }
+    if (e.keyCode === 13) {
+        createToDo();
+        AddCompleted()
+        DelFun()
+    }
 });
+// -------먼저 실행-----------
+AddCompleted()
+DelFun()
